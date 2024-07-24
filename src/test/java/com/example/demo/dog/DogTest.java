f74c00615fa0e87bda1493f736c2c737c89f4d5b
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-07-24 11:43:53 -0500

    progress

--- src/test/java/com/example/demo/dog/DogTest.java
+++ src/test/java/com/example/demo/dog/DogTest.java
@@ -1,8 +1,24 @@
 package com.example.demo.dog;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+// Other assertion functions in org.junit.jupiter.api.Assertions include:
+// assertAll (for executables), assertIterableEquals, assertLinesMatch, and assertNull.
 
 public class DogTest {
     @Test
@@ -11,7 +27,23 @@ public class DogTest {
         String name = "Comet";
         Dog dog = new Dog(breed, name);
         assertThat(dog.getBreed()).isEqualTo(breed);
-        assertThat(dog.getName()).isEqualTo(name);
+        assertEquals(name, dog.getName());
+        assertFalse(dog.getName().isEmpty());
+        assertNotEquals("Snoopy", dog.getName());
+        assertNotNull(dog.getName());
+        assertNotSame(breed, dog.getName());
+        assertSame(name, dog.getName());
+        assertTimeout(
+                Duration.ofMillis(1),
+                () -> new Dog(breed, name), // runs in same thread
+                "creating a Dog object is too slow"
+        );
+        assertTimeoutPreemptively(
+            Duration.ofMillis(1),
+            () -> new Dog(breed, name), // runs in new thread
+            "creating a Dog object is too slow"
+        );
+        assertTrue(dog.getName().length() > 4);
     }
 
     @Test
@@ -23,5 +55,14 @@ public class DogTest {
             () -> new Dog(breed, name),
             "Expected new Dog with empty breed to throw"
         );
+    }
+
+    @Test
+    public void otherAssertions() throws AssertionError {
+        String[] colors1 = {"red", "green", "blue"};
+        String[] colors2 = {"red", "green", "blue"};
+        assertArrayEquals(colors1, colors2);
+
+        //fail("should never reach this line");
     }
 }
